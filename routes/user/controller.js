fbab51528c2a3347008deee59cad7292112c9b1b
Jay200123 <kenken.ayonon@gmail.com>
2025-04-06 19:24:04 +0800

    Refactor UserController methods to include error handling and improve response structure

--- routes/user/controller.js
+++ routes/user/controller.js
@@ -1,21 +1,35 @@
+import { Handler, ErrorHandler } from "../../utils/index.js";
+
 export class UserController {
   constructor(UserService) {
     this.UserService = UserService;
   }
 
-  async getAllUsers(req, res) {
-    const results = await this.UserService.getAllUsersService();
-    return res.status(200).json({
-    data:results
-    });
+  async getAllUsers(req, res, next) {
+    try {
+      const results = new Handler(res);
+      const data = await this.UserService.getAllUsersService();
+
+      return results.SuccessHandler(200, data, "Users retrieved successfully");
+    } catch (err) {
+      return next(err);
+    }
   }
 
-  async getOneUser(req, res) {
-    const { id } = req.params;
-    const result = await this.UserService.getOneUserService(id);
-    return res.status(200).json({
+  async getOneUser(req, res, next) {
+    const results = new Handler(res);
+    const data = await this.UserService.getOneUserService(req.params.id);
+
+    return data.result.length === 0
+      ? next(new ErrorHandler(404, "User not found"))
+      : results.SuccessHandler(200, data, "User retrieved successfully");
+  }
+
+  async createUser(req, res, next) {
+    const data = await this.UserService.createUserService(req.body);
+    return res.status(201).json({
       status: "success",
-      data: result,
+      data,
     });
   }
 }
