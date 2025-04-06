140687563da5ec8323f8d99995176640ea8f8101
Jay200123 <kenken.ayonon@gmail.com>
2025-04-06 19:24:11 +0800

    Implement createUserRepository method to handle user creation in the database

--- routes/user/repository.js
+++ routes/user/repository.js
@@ -29,4 +29,27 @@ export class UserRepository {
       });
     });
   }
+
+  async createUserRepository(data) {
+    const { username, password, firstname, lastname, phone, address, city } =
+      data;
+    const QUERY = `
+    INSERT INTO users(username, 'password', 'date_created', 'date_modified')
+    VALUES(?, ?, NOW(), NOW());
+
+    INSERT INTO user_details(firstname, lastname, phone, address, city, date_created, date_modified, user_id)
+    VALUES(?, ?, ?, ?, ?, NOW(), NOW(), LAST_INSERT_ID());
+    `;
+
+    return new Promise((resolve, reject) => {
+      this.connection.query(
+        QUERY,
+        [username, password, firstname, lastname, phone, address, city],
+        (err, result) => {
+          if (err) throw reject(err);
+          resolve(result);
+        }
+      );
+    });
+  }
 }
